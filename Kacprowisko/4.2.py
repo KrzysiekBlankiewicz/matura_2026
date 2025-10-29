f8bd9290fda77b8c661f337736f23181d764ebdc
Kacper <83409600+Kacper246@users.noreply.github.com>
2025-10-29 21:33:44 +0100

    Delete Kacprowisko directory

--- Kacprowisko/4.2.py
+++
@@ -1,9 +0,0 @@
-file = open("odbiorcy.txt", "r").read().split()
-licznik = len(file)
-print(licznik)
-suma = 0
-for x in range(1,licznik+1):
-    print(x)
-    if str(x) not in file:
-        suma+=1
-print(suma)
--- Kacprowisko/4.4.py
+++
@@ -1,7 +0,0 @@
-file = open("odbiorcy.txt", "r").read().split()
-naj = 0
-for x in range(0,len(file)):
-    if file.count(file[x]) > int(file[naj]):
-        naj = x
-print(file[naj])
-
