d6092c1d34e5ae17c6ab211a087a497778f9ffd9
Jaxa111 <jkoziol379@gmail.com>
2025-11-06 00:46:20 +0100

    Update client.py

--- Jacek Tyzo/kasyno/client.py
+++ Jacek Tyzo/kasyno/client.py
@@ -6,6 +6,6 @@ client_socket.connect(("127.0.0.1", 65432))
 dane = input()
 client_socket.sendall(dane.encode())
 data = client_socket.recv(1024)
-print('Received', repr(data))
-balans = int(data.decode())
+print("Received:", data.decode())
 client_socket.close()
+
