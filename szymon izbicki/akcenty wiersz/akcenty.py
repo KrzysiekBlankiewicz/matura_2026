b4797132166c3be7c61fd706c25978b5675c35e5
szmn <124497035+SzymonIzbicki@users.noreply.github.com>
2026-01-16 11:28:15 +0100

    Update akcenty.py

--- szymon izbicki/akcenty wiersz/akcenty.py
+++ szymon izbicki/akcenty wiersz/akcenty.py
@@ -1,38 +1,53 @@
 def samogloski(tekst):
-    samogloski = ["a", "e", "o", "i", "y", "u", " "]
+    samogloski = ["a", "e", "o", "i", "y", "u"]
     wiersz = []
-    for i in range(len(tekst)):
-        if tekst[i] in samogloski:
-            wiersz.append(tekst[i])
-            if tekst[i+1] in samogloski: i += 1
 
+    i = 0
+    while i < len(tekst):
+        if tekst[i] == "\n":
+            wiersz.append("\n")
+            i += 1
+        elif tekst[i] == " ":
+            wiersz.append(" ")
+            i += 1
+        elif tekst[i] in samogloski:
+            wiersz.append(tekst[i])
+            if i + 1 < len(tekst) and tekst[i + 1] in samogloski:
+                i += 2
+            else:
+                i += 1
+        else:
+            i += 1
     return wiersz
 
-def akcenty(file):
-    #find accents
-    for i in range(len(file), 0, -1):
-        if file[i] == " ":
-            if file[i] == file[i-1]:
-                file[i-1] = "*"
-            else:
-                file[i-2] = "*"
 
-    #fill out the rest with "-" and remove spaces
-    for i in range(len(file)):
+def akcenty(file):
+    for i in range(len(file)-1, -1, -1):
+        if file[i] == "\n":
+            continue
+
+        if file[i] == " ":
+            if i - 1 >= 0 and file[i - 1] == " ":
+                file[i - 1] = "*"
+            else:
+                if i - 2 >= 0 and file[i - 2] != "\n":
+                    file[i - 2] = "*"
+
+    for i in range(len(file)-1,-1,-1):
         if file[i] == " ":
             del file[i]
-        elif file[i] != "*":
+        elif file[i] != "*" and file[i] != "\n":
             file[i] = "-"
 
     return file
 
+
 with open("lokomotywa.txt") as f:
     file = f.read()
+    file = file.replace("\n", " \n")
     file = list(file)
-    for i in range(len(file)):
-        if file[i] == "\n":
-            file[i] = " "
 
 file = samogloski(file)
 file = akcenty(file)
-print(file)
+
+print("".join(file))
